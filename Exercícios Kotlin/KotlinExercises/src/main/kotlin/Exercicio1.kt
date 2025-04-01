068de81cd1a315d00c6769ab1dd734744b9ce5d2
Eduardo Soares <eduardo_soares001@outlook.com>
2025-04-01 16:52:35 -0300

    Exercicio 1

--- Exercícios Kotlin/KotlinExercises/src/main/kotlin/Exercicio1.kt
+++ Exercícios Kotlin/KotlinExercises/src/main/kotlin/Exercicio1.kt
@@ -1,2 +1,16 @@
-class Exercicio2 {
+fun somar(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun main() {
+    val num1: Int
+    val num2: Int
+
+    print("Digite o PRIMEIRO número: ")
+    num1 = readln().toInt()
+
+    print("Digite o SEGUNDO número: ")
+    num2 = readln().toInt()
+
+    println("O resultado de $num1 + $num2 é: " + somar(num1, num2))
 }
