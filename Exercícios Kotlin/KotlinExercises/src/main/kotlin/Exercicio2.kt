891ffc2654557f7a781c23e8431cecef8ed6f4bd
Eduardo Soares <eduardo_soares001@outlook.com>
2025-04-01 17:03:11 -0300

    Exercicio 2

--- Exercícios Kotlin/KotlinExercises/src/main/kotlin/Exercicio2.kt
+++ Exercícios Kotlin/KotlinExercises/src/main/kotlin/Exercicio2.kt
@@ -1,3 +1,12 @@
+fun converter(metros: Double): Double{
+    return metros * 1000
+}
+
 fun main() {
-    println("Hello world")
+    val metros: Double
+
+    print("Digite o comprimento em metro(m) para ser convertido para milímetro(mm): ")
+    metros = readln().toDouble()
+
+    println("$metros metros são ${converter(metros)} milímetros.")
 }
